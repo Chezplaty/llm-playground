e16dc1dfba4acdd435d8f2ab66f64d4b0c041d25
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-08-30 16:26:59 -0700

    feat: assign each unique token an id

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ with open("training_data.txt", 'r', encoding="utf-8") as file:
     raw_text = file.read()
 
 result = re.split(r'([,.?_!"()\']|--|\s)', raw_text)
-result = [i for i in result if i.split()]
-print(len(result))
-print(result[:30])
+preprocessed = [i for i in result if i.split()]
+all_words = sorted(list(set(preprocessed)))
+
+#assign each unique token an id/number
+vocab = {token: i for i, token in enumerate(all_words)}
