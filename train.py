c9612aded44921f02f81579b1608af7169e10068
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-08-30 17:32:25 -0700

    fix: fixed minor bugs

--- train.py
+++ train.py
@@ -3,11 +3,12 @@ import re
 class SimpleTokenizer:
     def __init__(self, vocab: dict):
         self.str_to_int = vocab
-        self.int_to_str = {i: s for i, s in vocab.items()}
+        self.int_to_str = {i: s for s, i in vocab.items()}
 
     def encode(self, text) -> list[str]:
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
         preprocessed = [i for i in preprocessed if i.split()] #no white spaces
+        preprocessed = [i if i in self.str_to_int else "<|unk|>" for i in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
 
@@ -29,7 +30,7 @@ all_tokens.extend(["<|endoftext|>", "<|unk|>"])
 vocab = {token: i for i, token in enumerate(all_tokens)}
 
 tokenizer = SimpleTokenizer(vocab)
-text = "\"It's the last he painted, you know,\" Mrs. Gisburn said"
+text = "Cheese is cool, but it is way better grilled. <|endoftext|> Hello 8DD"
 ids = tokenizer.encode(text)
 print(ids)
 print(tokenizer.decode(ids))
