8e5eaaf65bfa521e48d64862dfe1e153ce12d1e8
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-08-30 17:10:09 -0700

    feat: created simple tokenizer class, mapping strings to ids based off sample text, need more data

--- train.py
+++ train.py
@@ -1,5 +1,22 @@
 import re
 
+class SimpleTokenizer:
+    def __init__(self, vocab: dict):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for i, s in vocab.items()}
+
+    def encode(self, text) -> list[str]:
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [i for i in preprocessed if i.split()] #no white spaces
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids: list[int]) -> str:
+        text = " ".join([self.int_to_str[num] for num in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+
 with open("training_data.txt", 'r', encoding="utf-8") as file:
     raw_text = file.read()
 
@@ -9,3 +26,9 @@ all_words = sorted(list(set(preprocessed)))
 
 #assign each unique token an id/number
 vocab = {token: i for i, token in enumerate(all_words)}
+
+tokenizer = SimpleTokenizer(vocab)
+text = "\"It's the last he painted, you know,\" Mrs. Gisburn said"
+ids = tokenizer.encode(text)
+print(ids)
+print(tokenizer.decode(ids))
