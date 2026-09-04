a0775318243586a32b335568f5c2d136e5a0c997
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-09-03 17:37:13 -0700

    replaced tokenizer logic with tiktoken module and established baseline for next-word predictions

--- train.py
+++ train.py
@@ -1,36 +1,18 @@
-import re
-
-class SimpleTokenizer:
-    def __init__(self, vocab: dict):
-        self.str_to_int = vocab
-        self.int_to_str = {i: s for s, i in vocab.items()}
-
-    def encode(self, text) -> list[str]:
-        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
-        preprocessed = [i for i in preprocessed if i.split()] #no white spaces
-        preprocessed = [i if i in self.str_to_int else "<|unk|>" for i in preprocessed]
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
-
-    def decode(self, ids: list[int]) -> str:
-        text = " ".join([self.int_to_str[num] for num in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
-        return text
-
+import tiktoken
 
 with open("training_data.txt", 'r', encoding="utf-8") as file:
     raw_text = file.read()
 
-result = re.split(r'([,.?_!"()\']|--|\s)', raw_text)
-preprocessed = [i for i in result if i.split()]
-all_tokens = sorted(list(set(preprocessed)))
-all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+tokenizer = tiktoken.get_encoding("gpt2")
 
-#assign each unique token an id/number
-vocab = {token: i for i, token in enumerate(all_tokens)}
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
 
-tokenizer = SimpleTokenizer(vocab)
-text = "Cheese is cool, but it is way better grilled. <|endoftext|> Hello 8DD"
-ids = tokenizer.encode(text)
-print(ids)
-print(tokenizer.decode(ids))
+enc_sample = enc_text[50:]
+
+context_size = 4
+
+for i in range(1, context_size + 1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(f"{tokenizer.decode(context)} ----> {tokenizer.decode(desired)}")
