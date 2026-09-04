863ceefe84ae620a66f05818880ee0db09c94138
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-09-03 17:38:01 -0700

    fix: used encode instead of decode

--- train.py
+++ train.py
@@ -15,4 +15,4 @@ context_size = 4
 for i in range(1, context_size + 1):
     context = enc_sample[:i]
     desired = enc_sample[i]
-    print(f"{tokenizer.decode(context)} ----> {tokenizer.decode(desired)}")
+    print(f"{tokenizer.encode(context)} ----> {tokenizer.encode(desired)}")
